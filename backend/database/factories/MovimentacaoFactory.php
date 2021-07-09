c0e7e392f89fe3f610589ca17a928aec14deda53
GenesesLopes <genesesslopes@gmail.com>
2021-07-09 04:32:04 +0000

    criando estrutura de Movimentacao

---
+++ backend/database/factories/MovimentacaoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Movimentacao;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MovimentacaoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Movimentacao::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
