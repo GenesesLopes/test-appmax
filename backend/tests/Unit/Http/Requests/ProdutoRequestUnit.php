91ce8647fa1d328c3afa427de5c76d8bc6ada6a7
GenesesLopes <genesesslopes@gmail.com>
2021-07-09 05:37:10 +0000

    Criando arquivo de testes unitários da request

---
+++ backend/tests/Unit/Http/Requests/ProdutoRequestUnit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit\Http\Requests;
+
+use Tests\TestCase;
+
+class ProdutoRequestUnit extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
