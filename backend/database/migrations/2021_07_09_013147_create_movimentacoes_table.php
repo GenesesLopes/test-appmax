3621784f77350347d67128e1745d9d292f259826
GenesesLopes <genesesslopes@gmail.com>
2021-07-09 04:57:04 +0000

    consertando estrutura de banco de movimentacoes

--- backend/database/migrations/2021_07_09_013147_create_movimentacoes_table.php
+++ backend/database/migrations/2021_07_09_013147_create_movimentacoes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMovimentacaosTable extends Migration
+class CreateMovimentacoesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,12 @@ class CreateMovimentacaosTable extends Migration
      */
     public function up()
     {
-        Schema::create('movimentacaos', function (Blueprint $table) {
+        Schema::create('movimentacoes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('estoque_produtos_id')->constrained('estoques','produto_id');
+            $table->integer('quantidade');
+            $table->string('acao',45);
+            $table->string('origem',45);
             $table->timestamps();
         });
     }
@@ -26,6 +30,6 @@ class CreateMovimentacaosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('movimentacaos');
+        Schema::dropIfExists('movimentacoes');
     }
 }
