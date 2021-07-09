4fcbd3ebd06e1a342ca37ce7449d6b7ab9ec6e7a
GenesesLopes <genesesslopes@gmail.com>
2021-07-09 04:19:34 +0000

    estrutura estoque

---
+++ backend/database/migrations/2021_07_09_011535_create_estoques_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstoquesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estoques', function (Blueprint $table) {
+            $table->foreignId('produtos_id')->constrained('produtos');
+            $table->unsignedInteger('quantidade');
+            $table->softDeletes();
+            $table->timestamps();
+            $table->primary('produtos_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estoques');
+    }
+}
