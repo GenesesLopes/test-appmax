1b612130da6bf8ce20598940b183c0605b3b5130
GenesesLopes <genesesslopes@gmail.com>
2021-07-09 04:25:16 +0000

    Definições de Model

--- backend/app/Models/Estoque.php
+++ backend/app/Models/Estoque.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Estoque extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['produto_id','quantidade'];
+    protected $dates = ['deleted_at'];
+
+    protected $casts = [
+        'quantidade'=> 'integer'
+    ];
+
+    protected $primaryKey = 'produto_id';
+
+    public $incrementing = false;
 }
