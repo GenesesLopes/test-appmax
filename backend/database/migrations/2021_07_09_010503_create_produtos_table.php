08ef1dbee03894bf41ed1453ad5ae0395799fba4
GenesesLopes <genesesslopes@gmail.com>
2021-07-09 04:08:08 +0000

    Tabela Produtos

--- backend/database/migrations/2021_07_09_010503_create_produtos_table.php
+++ backend/database/migrations/2021_07_09_010503_create_produtos_table.php
@@ -15,6 +15,8 @@ class CreateProdutosTable extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->string('sku');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
