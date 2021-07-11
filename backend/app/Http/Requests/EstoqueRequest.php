397565b163f974294cd6ccad6548fa868244add0
GenesesLopes <genesesslopes@gmail.com>
2021-07-11 17:24:36 +0000

    Validando quantidade minima de produto

--- backend/app/Http/Requests/EstoqueRequest.php
+++ backend/app/Http/Requests/EstoqueRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Repositories\Contracts\IEstoque;
 use App\Repositories\Contracts\IProduto;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Validator;
@@ -11,9 +12,10 @@ class EstoqueRequest extends FormRequest
 
     public function __construct(
         public IProduto $iProduto,
+        public IEstoque $iEstoque,
         array $query = [],
         array $request = []
-    ){
+    ) {
         parent::__construct(query: $query, request: $request);
     }
     /**
@@ -35,18 +37,20 @@ class EstoqueRequest extends FormRequest
     {
         return [
             'produto_id' => 'required|integer',
-            'quantidade' => 'required|integer'
+            'quantidade' => 'required|integer|gt:0'
         ];
     }
     public function withValidator(Validator $validator)
     {
         $validator->after(function (Validator $validator) {
+
             if (!$validator->errors()->count()) {
-                if ($this->quantidade === 0) {
-                    $validator->errors()->add('quantidade', 'A quantidade deve ser diferente de 0');
-                }
-                 else if ($this->iProduto->find($this->produto_id) == null) {
+                if ($this->iProduto->find($this->produto_id) == null) {
                     $validator->errors()->add('produto_id', 'Produto não encontrado');
+                } else if ($this->isMethod('PUT')) { // Validação para baixa de produtos
+                    if ($this->iEstoque->countQuantidade($this->produto_id) < $this->quantidade) {
+                        $validator->errors()->add('quantidade', 'Quantidade a ser removida deve ser igual ou superior à quantidade em estoque');
+                    }
                 }
             }
         });
